23bdc79e55a1b927aeb8e1765b0bd4fade663702
z <z@xnz.me>
2016-04-15 19:12:49 -0400

    code cleanup and showing more data

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -20,36 +20,70 @@ $(document).ready(function () {
     return into;
   }
 
+  // Handle tabs on page reload
+  function handleTabs() {
+      // Javascript to enable link to tab
+      var url = document.location.toString();
+      if (url.match('#')) {
+          $('.navbar-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+      }
+
+      // Change hash for page-reload
+      $('.navbar-tabs a').on('shown.bs.tab', function (e) {
+          history.pushState( null, null, $(this).attr('href') );
+          window.location.hash = e.target.hash;
+      });
+  }
+
+  handleTabs();
+
   // Get the stats and filter them down.
   $.get('resources/data/stats.json', function(response) {
 
     var stats_data = response.data;
 
-    var c3data = [];
-
-    function newArray(element, index, array) {
+    // Need to do this until: https://github.com/masayuki0812/c3/issues/1471
+    function flattenCountries(element, index, array) {
       element.time = element.time * 1000;
       c3data.push(flatten(element));
     }
 
-    stats_data.forEach(newArray);
+    var c3data = [];
+    stats_data.forEach(flattenCountries);
+
+    var countryList = [
+      'countries_CA',
+      'countries_UA',
+      'countries_US',
+      'countries_FR',
+      'countries_DE',
+      'countries_AU',
+      'countries_RU',
+      'countries_GB',
+      'countries_ZA',
+      'countries_CL'
+    ];
+
+    var countryTypes = {};
+    for (var i = 0; i < countryList.length; i++) {
+      countryTypes[countryList[i]] = 'area-spline';
+    }
+
+    countryTypes['total_players'] = 'line';
+
+    var xKeys = countryList.slice();
+    xKeys.push('total_players');
 
     var chart = c3.generate({
       bindto: '#chart-players',
       data: {
         json: c3data,
         keys: {
-          value: ['countries_CA', 'countries_UA', 'countries_US', 'countries_FR', 'countries_DE'],
+          value: xKeys,
           x: 'time'
         },
-        types: {
-            countries_CA: 'area-spline',
-            countries_UA: 'area-spline',
-            countries_US: 'area-spline',
-            countries_FR: 'area-spline',
-            countries_DE: 'area-spline'
-        },
-        groups: [['countries_CA', 'countries_UA', 'countries_US', 'countries_FR', 'countries_DE']]
+        types: countryTypes,
+        groups: [countryList]
       },
       axis: {
         x: {
