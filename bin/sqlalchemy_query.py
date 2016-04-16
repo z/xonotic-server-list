165cdb38a5a4612e17149bff4f77399ae5d89d0a
z <z@xnz.me>
2016-04-16 09:09:50 -0400

    return all as JSON

--- bin/sqlalchemy_query.py
+++ bin/sqlalchemy_query.py
@@ -1,6 +1,8 @@
 from sqlalchemy_base import Stats, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import calendar
+from datetime import datetime
 
 engine = create_engine('sqlite:///resources/data/stats.db')
 Base.metadata.bind = engine
@@ -9,7 +11,21 @@ DBSession = sessionmaker()
 DBSession.bind = engine
 session = DBSession()
 
-session.query(Stats).all()
+stats = session.query(Stats).all()
 
-stats = session.query(Stats).first()
-print(stats.total_players)
+json_out = [];
+
+for row in stats:
+
+    unix_time = calendar.timegm(row.time.utctimetuple())
+
+    datapoint = {
+        'time': unix_time,
+        'total_players': row.total_players,
+        'total_bots': row.total_bots,
+        'countries': row.countries,
+    }
+
+    json_out.append(datapoint)
+
+print(json_out)
